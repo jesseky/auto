c04360f48c22fde86dcbac4837f67bfc18c68ff8
jesseky <xiangaoji@gmail.com>
2020-09-02 11:41:39 +0800

    set-ntp

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # auto
 Some automated scripts
+
+# Usage 
+
+## 使用说明
+
+设置ntpdate：时间自动更新服务
+
+```bash
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/jesseky/auto/master/set-ntp.sh)"
+```
---
+++ set-ntp.sh
@@ -0,0 +1,22 @@
+echo "[1] install ntpdate..."
+
+yum install ntpdate -y
+
+echo "[2] update time..."
+ntpdate 1.ro.pool.ntp.org
+
+
+echo "[3] set timesyncd..."
+echo '[Time]' >> /etc/systemd/timesyncd.conf
+echo 'NTP=0.ro.pool.ntp.org 1.ro.pool.ntp.org' >> /etc/systemd/timesyncd.conf
+echo 'FallbackNTP=ntp.ubuntu.com 0.arch.pool.ntp.org' >> /etc/systemd/timesyncd.conf
+
+echo "[4] set-ntp..."
+timedatectl set-ntp true 
+timedatectl status
+mv /etc/localtime /etc/localtime.backup
+ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+
+echo "\n"
+date
+echo "[5] time set done"
